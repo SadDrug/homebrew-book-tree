8cdb8ac80197b3bd4283840cd0233f7b3745179e
SadDrug <hopefulcrook@icloud.com>
2022-03-21 23:09:54 -0600

    adjusted boost version names

--- Formula/boost@1.67.rb
+++ Formula/boost@1.67.rb
@@ -1,4 +1,4 @@
-class Boost < Formula
+class BoostAT167 < Formula
   desc "Collection of portable C++ source libraries"
   homepage "https://www.boost.org/"
   url "https://dl.bintray.com/boostorg/release/1.67.0/source/boost_1_67_0.tar.bz2"
